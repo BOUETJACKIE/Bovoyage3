686e5e5dabfd79d9344163b22fc4e5d3edd1f9d9
jackie <jackiebouet@hotmail.fr>
2018-07-10 11:32:55 +0200

    création de dbcontext

---
+++ WebApplication1/WebApplication1/Data/Bovoyage3DbContext.cs
@@ -0,0 +1,30 @@
+﻿using Bovoyage3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace Bovoyage3.Data
+{
+    public class Bovoyage3DbContext: DbContext
+    {
+        public Bovoyage3DbContext() : base("bovoyage3") { }
+            
+        public DbSet<Participant> Participants { get; set; }
+
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Assurance> Assurances { get; set; }
+
+        public DbSet<AgenceVoyage> AgenceVoyages { get; set; }
+
+        public DbSet<Destination> Destinations { get; set; }
+
+        public DbSet<DossierReservation> DossierReservations { get; set; }
+
+        public DbSet<Voyage> Voyages { get; set; }
+
+    }
+}
--- WebApplication1/WebApplication1/Properties/AssemblyInfo.cs
+++ WebApplication1/WebApplication1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("WebApplication1")]
+[assembly: AssemblyTitle("Bovoyage3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebApplication1")]
+[assembly: AssemblyProduct("Bovoyage3")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
