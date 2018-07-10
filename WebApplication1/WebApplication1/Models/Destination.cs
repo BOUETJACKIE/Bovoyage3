65844e9c3a84d173b252763fc32e4cfb575d16c3
User1 <antoine_352@hotmail.fr>
2018-07-10 10:05:36 +0200

    Création class Destination

---
+++ WebApplication1/WebApplication1/Models/Destination.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Destination
+    {
+        public string Continent { get; set; }
+        public string Pays { get; set; }
+        public string Region { get; set; }
+        public string description { get; set; }
+    }
+}
