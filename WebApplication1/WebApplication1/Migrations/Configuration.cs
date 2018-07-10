e7f00d83a37464594a047c6dabefc5e478bc6f89
Vincent DEISZ <vincentdeisz@hotmail.fr>
2018-07-10 12:29:49 +0200

    Création class Configuration (migrations)

---
+++ WebApplication1/WebApplication1/Migrations/Configuration.cs
@@ -0,0 +1,15 @@
+namespace Bovoyage3.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Bovoyage3.Data.Bovoyage3DbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+    }
+}
